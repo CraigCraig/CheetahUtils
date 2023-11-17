428bea3979466e479f600af88e08dc53ca9f54b2
Craig Craig <craig@craig.se>
2023-11-17 05:09:19 -0600

    Add ArrayExtensions

---
+++ ArrayExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace CheeseyUtils;
+
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Converts all strings in the array to lowercase
+    /// </summary>
+    /// <param name="list"></param>
+    public static void ToLower(this string[] list)
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            list[i] = list[i].ToLower();
+        }
+    }
+
+    // Add a method to convert all strings in the array to uppercase
+    /// <summary>
+    /// Converts all strings in the array to uppercase
+    /// </summary>
+    public static void ToUpper(this string[] list)
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            list[i] = list[i].ToUpper();
+        }
+    }
+}
