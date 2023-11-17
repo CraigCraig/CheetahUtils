8358fb11df79c0ca025a58ad58f90fe839e8cc68
Craig Craig <craig@craig.se>
2023-11-17 08:54:55 -0600

    More Extensions

---
+++ Extensions/Strings.cs
@@ -0,0 +1,14 @@
+﻿namespace CheeseyUtils.Extensions;
+
+public static class Strings
+{
+    public static string ToTitleCase(this string str)
+    {
+        string[] words = str.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1).ToLower();
+        }
+        return string.Join(' ', words);
+    }
+}
