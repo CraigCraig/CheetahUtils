19db1d462c557e60cbe62087f555e2cb2317d001
Craig Craig <craig@craig.se>
2023-11-21 00:50:46 -0600

    Moved Terminal.cs to CheetahUtils

---
+++ Terminal.cs
@@ -0,0 +1,18 @@
+﻿namespace CheeseyUtils;
+
+using System.Text;
+using CliWrap;
+
+public static class Terminal
+{
+	public static string Execute(string command, string[] args)
+	{
+		StringBuilder sb = new();
+		var c = Cli.Wrap(command).WithArguments(args)
+			.WithStandardErrorPipe(PipeTarget.ToDelegate((s) => sb.AppendLine(s)))
+			.WithStandardOutputPipe(PipeTarget.ToDelegate((s) => sb.AppendLine(s)));
+
+		var result = c.ExecuteAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+		return sb.ToString();
+	}
+}
