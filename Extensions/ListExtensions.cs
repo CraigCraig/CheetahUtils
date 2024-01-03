7220124661a45d427a48eb029f5ad3202ae75552
Craig Craig <craig@craig.se>
2024-01-03 05:09:38 -0600

    Update Project Files

---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace CheetahUtils.Extensions;
+public static class ListExtensions
+{
+    public static List<string> ToUpperCase(this List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i] = list[i].ToUpper();
+        }
+        return list;
+    }
+
+    public static List<string> ToLowerCase(this List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i] = list[i].ToLower();
+        }
+        return list;
+    }
+
+    public static List<string> ToTitleCase(this List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i] = list[i].ToTitleCase();
+        }
+        return list;
+    }
+
+    public static List<string> Shuffle(this List<string> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = new Random().Next(n + 1);
+            (list[n], list[k]) = (list[k], list[n]);
+        }
+        return list;
+    }
+}
